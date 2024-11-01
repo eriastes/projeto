c93e682b856b1faf4ea591fc557c13d0495773d4
eriastes <erikaprestes7@gmail.com>
2024-11-01 16:29:46 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let nomeusuario = "";
+let elemento = document.querySelector("#nome-usuario")
+
+
+while(nome-usuario==""){
+    prompt("qual o seu nome?" );
+
+}
+
+if(nomeusuario==null){
+    elemento.textContent = "seja muito bem vindo.";
+}else{
+    elemento.textContent = nomeusuario;
+}
